cfb70f276086cfc4ec346307f77a747c3b79b2b2
Elyse Ando <elysekando@gmail.com>
2022-09-27 01:35:49 -0400

    Completed Exercise02

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -73,12 +73,15 @@ public class Silly implements Comparable<Silly>{
     }
 
     /**
-     * TODO (Task 1): Create another constructor that takes in TWO parameters,
-     *       both of which are strings.
-     *       Afterwards, set this.name to the concatenation of both
-     *       strings (e.g. this.name = [first string] + [second string]).
-     *       Make sure you document this method!
+     * Creates a new Silly object.
+     * This constructor takes in a two Strings as an argument.
+     *
+     * @param s1 a name for this Silly instance's name.
+     * @param s2 a name for this Silly instance's name.
      */
+    public Silly(String s1, String s2){
+        this.name = s1 + s2;
+    }
 
 
 
@@ -116,7 +119,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
@@ -133,7 +136,7 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public String toString(){
-        // TODO (Task 3): Implement the body of this method!
+        return this.name;
     }
 
     /**
@@ -161,6 +164,7 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+        return this.name.equals(other.name);
     }
 
     /**
@@ -185,15 +189,14 @@ public class Silly implements Comparable<Silly>{
      */
     @Override
     public int compareTo(Silly other) {
-        /**
-         * TODO (Task 5): Implement the body of this method.
-         *                A positive number should be returned if this.name
-         *                is longer than other.name, a negative number if
-         *                other.name is longer than this.name, and 0 if
-         *                the lengths are equal.
-         *                You can get the length of a string by using the
-         *                .length() method.
-         */
+        if (this.name.length() < other.name.length()) {
+            return -1;
+        } else if (this.name.length() > other.name.length()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
     }
 
     /*
@@ -205,5 +208,5 @@ public class Silly implements Comparable<Silly>{
      *                If the tests don't pass, look at the results and revise
      *                accordingly.
      */
-}
+
 
